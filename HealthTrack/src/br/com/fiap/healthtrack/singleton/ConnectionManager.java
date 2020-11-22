b208b7fbb95bc0bf3026f7e17a1b76573ccae1eb
jose barreto teixeira de paula <jose.barreto@fieldcorp.com.br>
2020-11-22 02:39:29 -0300

    build

--- HealthTrack/src/br/com/fiap/healthtrack/singleton/ConnectionManager.java
+++ HealthTrack/src/br/com/fiap/healthtrack/singleton/ConnectionManager.java
@@ -23,8 +23,8 @@ public class ConnectionManager {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM84776",
-					"110994");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "****",
+					"****");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
